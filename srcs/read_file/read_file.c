d7b467f7c071c2197dfd3f5474e7b604a5cfc024
shima8823 <taiyaki1275@gmail.com>
2022-11-13 13:10:12 +0900

    add: map mode

--- srcs/read_file/read_file.c
+++ srcs/read_file/read_file.c
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   file_parser.c                                      :+:      :+:    :+:   */
+/*   read_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:18:23 by shima             #+#    #+#             */
-/*   Updated: 2022/11/11 16:19:15 by shima            ###   ########.fr       */
+/*   Updated: 2022/11/13 13:06:15 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-int	count_array(char **split_line);
 void	read_file(t_game_info *info, char *path);
 void	line_parse(t_game_info *info, t_file_parse *fp, char *line);
 void	init_file_parse(t_file_parse *fp);
+bool	is_empty_line(char *line);
+void	set_player_dir(t_game_info *info, double *dir_info);
+void	init_dir_info(double (*dir_info)[4][4]);
 
 void	read_file(t_game_info *info, char *path)
 {
@@ -37,18 +39,34 @@ void	read_file(t_game_info *info, char *path)
 		line_parse(info, &fp, line);
 		free(line);
 	}
-	printf("NO: [%s]\n", info->texture_path[0]);
-	printf("SO: [%s]\n", info->texture_path[1]);
-	printf("WE: [%s]\n", info->texture_path[2]);
-	printf("EA: [%s]\n", info->texture_path[3]);
+	close(fp.fd);
+
+	if (!info->texture_path[N]
+		|| !info->texture_path[S]
+		|| !info->texture_path[W]
+		|| !info->texture_path[E]
+		|| !info->floor_ceiling_color[0]
+		|| !info->floor_ceiling_color[1]
+		|| info->spawn_dir == 0
+		|| fp.exists_nl_end_of_map)
+		error_exit(ERR_MSG_INVALID_ID);
+	info->floor_color = get_color(info->floor_ceiling_color[0]);
+	info->ceiling_color = get_color(info->floor_ceiling_color[1]);
+	printf("NO: [%s]\n", info->texture_path[N]);
+	printf("SO: [%s]\n", info->texture_path[S]);
+	printf("WE: [%s]\n", info->texture_path[W]);
+	printf("EA: [%s]\n", info->texture_path[E]);
 	printf("F: [%s]\n", info->floor_ceiling_color[0]);
 	printf("C: [%s]\n", info->floor_ceiling_color[1]);
-	close(fp.fd);
+	printf("pos: %f %f\n", info->posX, info->posY);
+	printf("spawn_dir: %c\n", info->spawn_dir);
 }
 
 void	init_file_parse(t_file_parse *fp)
 {
 	fp->state = NEUTRAL;
+	fp->skip_empty_line = false;
+	fp->exists_nl_end_of_map = true;
 	fp->texture_index = 0;
 	fp->floor_ceiling_index = 0;
 	ft_strlcpy(fp->texture_id[0], "NO", 3);
@@ -63,14 +81,14 @@ void	line_parse(t_game_info *info, t_file_parse *fp, char *line)
 {
 	char	**split_line;
 	char	*line2;
-	// skip_line
-	if (ft_strlen(line) == 1 && ft_strchr(line, '\n'))
+
+	if (fp->skip_empty_line && is_empty_line(line))
 		return ;
 	if (fp->state == NEUTRAL)
 	{
 		split_line = ft_split(line, ' ');
 		if (count_array(split_line) != 2)
-			error_exit(ERR_MSG_LINE_INFO_NUM);
+			error_exit(ERR_MSG_INVALID_ID);
 		if (ft_strncmp(split_line[0], "NO", 3) == 0)
 			fp->state = TEXTURE_MODE;
 		else if (ft_strncmp(split_line[0], "F", 2) == 0)
@@ -80,9 +98,10 @@ void	line_parse(t_game_info *info, t_file_parse *fp, char *line)
 	}
 	if (fp->state == TEXTURE_MODE)
 	{
+		fp->skip_empty_line = false;
 		split_line = ft_split(line, ' ');
 		if (count_array(split_line) != 2)
-			error_exit(ERR_MSG_LINE_INFO_NUM);
+			error_exit(ERR_MSG_INVALID_ID);
 		if (ft_strncmp(split_line[0], fp->texture_id[fp->texture_index], 3) == 0)
 		{
 			if (info->texture_path[fp->texture_index])
@@ -103,15 +122,17 @@ void	line_parse(t_game_info *info, t_file_parse *fp, char *line)
 			else
 				fp->state = MAP_MODE;
 			line2 = get_next_line(fp->fd);
-			if (ft_strlen(line2) != 1 || !ft_strchr(line2, '\n'))
+			if (!is_empty_line(line2))
 				error_exit(ERR_MSG_INVALID_ID);
+			fp->skip_empty_line = true;
 		}
 	}
 	else if (fp->state == COLOR_MODE)
 	{
+		fp->skip_empty_line = false;
 		split_line = ft_split(line, ' ');
 		if (count_array(split_line) != 2)
-			error_exit(ERR_MSG_LINE_INFO_NUM);
+			error_exit(ERR_MSG_INVALID_ID);
 		if (ft_strncmp(split_line[0], fp->floor_ceiling_id[fp->floor_ceiling_index], 2) == 0)
 		{
 			if (info->floor_ceiling_color[fp->floor_ceiling_index])
@@ -132,19 +153,97 @@ void	line_parse(t_game_info *info, t_file_parse *fp, char *line)
 			else
 				fp->state = MAP_MODE;
 			line2 = get_next_line(fp->fd);
-			if (ft_strlen(line2) != 1 || !ft_strchr(line2, '\n'))
+			if (!is_empty_line(line2))
 				error_exit(ERR_MSG_INVALID_ID);
+			fp->skip_empty_line = true;
 		}
 	}
-	
+	else if (fp->state == MAP_MODE)
+	{
+		size_t	i;
+
+		fp->skip_empty_line = false;
+		if (is_empty_line(line))
+			error_exit(ERR_MSG_INVALID_ID);
+		if (!ft_strchr(line, '\n'))
+			fp->exists_nl_end_of_map = false;
+		i = 0;
+		while (line[i])
+		{
+			if (!ft_strchr(" 01NSEW\n", line[i]))
+				error_exit(ERR_MSG_INVALID_CHAR);
+			if (ft_strchr("NSEW", line[i]))
+			{
+				if (info->spawn_dir != 0)
+					error_exit(ERR_MSG_MULTIPLE_SPAWN);
+				info->spawn_dir = line[i];
+				info->posY = i;
+				info->posX = info->map_size_y;
+				line[i] = '0';
+			}
+			else if (line[i] == '\n')
+			{
+				line[i] = '\0';
+				break ;
+			}
+			i++;
+		}
+		ft_strlcpy(info->map[info->map_size_y], line, MAP_X_LIMIT);
+		info->map_size_y++;
+		if (i - 1 > MAP_X_LIMIT - 1 || info->map_size_y - 1> MAP_Y_LIMIT - 1)
+			error_exit(ERR_MSG_MAP_SIZE);
+	}
 }
 
-int	count_array(char **split_line)
+bool	is_empty_line(char *line)
 {
-	int	i;
+	if (!line)
+		return (false);
+	return (ft_strlen(line) == 1 && ft_strchr(line, '\n'));
+}
 
-	i = 0;
-	while (split_line[i])
-		i++;
-	return (i);
+void	init_player_dir(t_game_info *info)
+{
+	char	c;
+	double	dir_info[4][4];
+
+	init_dir_info(&dir_info);
+	c = info->spawn_dir;
+	if (c == 'N')
+		set_player_dir(info, dir_info[N]);
+	else if (c == 'S')
+		set_player_dir(info, dir_info[S]);
+	else if (c == 'W')
+		set_player_dir(info, dir_info[W]);
+	else if (c == 'E')
+		set_player_dir(info, dir_info[E]);
+		
+}
+
+void	init_dir_info(double (*dir_info)[4][4])
+{
+	(*dir_info)[N][DIR_X] = -1.0;
+	(*dir_info)[N][DIR_Y] = 0.0;
+	(*dir_info)[N][PLANE_X] = 0.0;
+	(*dir_info)[N][PLANE_Y] = 0.66;
+	(*dir_info)[S][DIR_X] = 1.0;
+	(*dir_info)[S][DIR_Y] = 0.0;
+	(*dir_info)[S][PLANE_X] = 0.0;
+	(*dir_info)[S][PLANE_Y] = -0.66;
+	(*dir_info)[W][DIR_X] = 0.0;
+	(*dir_info)[W][DIR_Y] = -1.0;
+	(*dir_info)[W][PLANE_X] = -0.66;
+	(*dir_info)[W][PLANE_Y] = 0.0;
+	(*dir_info)[E][DIR_X] = 0.0;
+	(*dir_info)[E][DIR_Y] = 1.0;
+	(*dir_info)[E][PLANE_X] = 0.66;
+	(*dir_info)[E][PLANE_Y] = 0.0;
+}
+
+void	set_player_dir(t_game_info *info, double *dir_info)
+{
+	info->dirX = dir_info[DIR_X];
+	info->dirY = dir_info[DIR_Y];
+	info->planeX = dir_info[PLANE_X];
+	info->planeY = dir_info[PLANE_Y];
 }
