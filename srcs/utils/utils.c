ba3e1f4f32a7cab70ee59325956e05c836f02d78
shima8823 <taiyaki1275@gmail.com>
2022-11-13 12:57:35 +0900

    add: count_array func

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:28:55 by shima             #+#    #+#             */
-/*   Updated: 2022/11/11 16:29:26 by shima            ###   ########.fr       */
+/*   Updated: 2022/11/13 07:16:38 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,14 @@ void	error_exit(char *err_msg)
 	ft_putendl_fd("Error", STDERR_FILENO);
 	ft_putendl_fd(err_msg, STDERR_FILENO);
 	exit(EXIT_FAILURE);
+}
+
+int	count_array(char **split_line)
+{
+	int	i;
+
+	i = 0;
+	while (split_line[i])
+		i++;
+	return (i);
 }
