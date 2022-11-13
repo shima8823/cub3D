d4facc82182601d3bd4c9895aa5cfc180073c922
shima8823 <taiyaki1275@gmail.com>
2022-11-13 13:07:11 +0900

    add: test

---
+++ test/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+WHITE="\033[37m"
+
+SHELL_DIR="`dirname $0`"
+CUBFILE_DIR="assets/cubfiles/invalid"
+CUBEXEC_DIR="."
+
+# もしshellが"test/"で呼ばれているならば
+if [ "." == "$SHELL_DIR" ]; then
+	CUBFILE_DIR="../assets/cubfiles/invalid"
+	CUBEXEC_DIR=".."
+fi
+
+function test_file_parse()
+{
+	$CUBEXEC_DIR/cub3D $1 2> /dev/null
+	if [ $? -eq "1" ]; then
+		printf "$GREEN[$1]\n"
+	else
+		printf "$RED[$1]\n"
+	fi
+}
+
+for file in $(ls $CUBFILE_DIR/*); do
+	test_file_parse $file
+done
