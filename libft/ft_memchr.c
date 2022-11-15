ca3a51d855497e8e7266935e586d7735bf04d736
shima8823 <taiyaki1275@gmail.com>
2022-11-15 10:09:12 +0900

    fix: norm error

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 17:22:39 by shima             #+#    #+#             */
-/*   Updated: 2022/11/01 19:35:55 by shima            ###   ########.fr       */
+/*   Updated: 2022/11/15 10:08:41 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	unsigned char *us;
+	size_t			i;
+	unsigned char	*us;
 
 	us = (unsigned char *)s;
 	i = 0;
